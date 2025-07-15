8cad37ab586b73b16bd7201cd6747f53f5063e04
RubenBoone <ruben.boone01@gmail.com>
2025-07-15 20:43:24 +0200

    build test

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <button onClick={async () => {
-          let answer = await fetch('http://172.29.0.22:7001/api/test');
+          let answer = await fetch('https://bakery.dontask.dev/api/test');
           console.log(await answer.json())
         }}>
           test backend
