5542e0b63188892637c44c29688ccd85a1d10c6b
RubenBoone <ruben.boone01@gmail.com>
2025-07-15 20:49:19 +0200

    build test

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Client-side routing fallback
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
